92f5ca9be23aab4b1f93fb81375bdd4aac5ca525
木叶ran <2501590635@qq.com>
2020-02-01 14:08:13 +0800

    fix

---
+++ algorithm/algorithmcode/删除最外层括号.h
@@ -0,0 +1,33 @@
+#ifdef Bracket_Matching
+#define BRACKET_MATCHING
+#include<string>
+#include<stack>
+using namespace std;
+
+class Bracket
+{
+    public:
+   //1021. É¾³ý×îÍâ²ãµÄÀ¨ºÅ
+    string removeOuterParentheses(string S){
+        int L=1,R=0;
+        string st;
+        for(int i=1;i<S.size();i++){
+            if(S.at(i)=='(')
+                L++;
+            else{
+                R++;
+            }
+
+            if(L!=R)
+                st.push_back(S.at(i));
+            else{
+                i++;
+                L=1;
+                R=0;
+            }
+        }
+        return st;
+    }
+};
+
+#endif
--- algorithm/algorithmcode/括号匹配.h
+++ algorithm/algorithmcode/括号匹配.h
@@ -1,4 +1,4 @@
-#ifdef Bracket_Matching
+ï»¿#ifdef Bracket_Matching
 #define BRACKET_MATCHING
 #include<string>
 #include<stack>
@@ -8,7 +8,7 @@ class Bracket
 {
     public:
 
-    //À¨ºÅÆ¥Åä
+    //æ‹¬å·åŒ¹é…
     bool isValid(string s){
         stack<char>sta;
         if(s.empty())
@@ -28,28 +28,6 @@ class Bracket
             }
         }
         return sta.empty();
-    }
-
-    //1021. É¾³ı×îÍâ²ãµÄÀ¨ºÅ
-    string removeOuterParentheses(string S){
-        int L=1,R=0;
-        string st;
-        for(int i=1;i<S.size();i++){
-            if(S.at(i)=='(')
-                L++;
-            else{
-                R++;
-            }
-
-            if(L!=R)
-                st.push_back(S.at(i));
-            else{
-                i++;
-                L=1;
-                R=0;
-            }
-        }
-        return st;
     }
 };
 
